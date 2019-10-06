8970b7cad450adfbded38aa63dda7ed8bc5b0745
Khotso Charles <Mokhetkc@hotmail.com>
2019-10-06 15:13:18 +0200

    Sticking to the instructors instruction

--- src/app/events/event-thumbnail/event-thumbnail/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail/event-thumbnail/event-thumbnail.component.ts
@@ -1,15 +1,33 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input } from '@angular/core'
 
 @Component({
-  // tslint:disable-next-line: component-selector
-  selector: "event-thumbnail",
-  templateUrl: "./event-thumbnail.component.html",
-  styleUrls: ["./event-thumbnail.component.css"]
+  selector: 'event-thumbnail',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{event?.name}}</h2>
+      <div>Date: {{event?.date}}</div>
+      <div [ngStyle]="getStartTimeStyle()" [ngSwitch]="event?.time">Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{event?.price}}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{event?.location?.address}}</span>
+        <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{event?.onlineUrl}}
+      </div>
+    </div>
+  `
 })
 export class EventThumbnailComponent {
-  @Input() events: any;
-  @Output() eventClick = new EventEmitter();
-  handleclickMe() {
-    this.eventClick.emit(this.events.name);
+  @Input() event:any
+
+  getStartTimeStyle():any {
+    if (this.event && this.event.time === '8:00 am')
+      return {color: '#003300', 'font-weight': 'bold'}
+    return {}
   }
 }
