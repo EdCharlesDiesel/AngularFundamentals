a4b7c58058a84360350129dce50f902c14ba023f
Khotso Charles <Mokhetkc@hotmail.com>
2019-10-06 02:11:02 +0200

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
