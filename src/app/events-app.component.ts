be05d632aced2db708b0da275b79936bf28b9253
Khotso Charles <Mokhetkc@hotmail.com>
2019-10-06 02:20:25 +0200

    removed space

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template:'<h2>Hello World</h2>'
+  template: '<h2>Hello World</h2>'
 
 })
 export class EventsAppComponent {
